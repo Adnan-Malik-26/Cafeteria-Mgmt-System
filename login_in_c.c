b3fb90f25d390f314eb23b43378413e23feec37d
adnan-malik <eddymalikadnan@gmail.com>
2023-02-11 14:28:10 +0530

    My Changes

--- login_in_c.c
+++ login_in_c.c
@@ -7,11 +7,11 @@ int main()
     int order,choice;
     int total=0;
     int no_of_items;
-    int chow=0,pizza=0,pasta=0,momos=0,rice=0;
-    int pizza_total,pasta_total,momos_total,chowmein_total,fried_rice_total;
+    int chow=0,pizza=0,pasta=0,momos=0,rice=0,honey=0;
+    int pizza_total,pasta_total,momos_total,chowmein_total,fried_rice_total,honey_total;
     
     login:
-    printf("LOGIN PAGE");
+    printf("*********LOGIN PAGE*********\n");
     printf("Username: ");
     scanf("%s",&username);
     printf("Password: ");
@@ -30,55 +30,64 @@ int main()
     printf("**********WELCOME TO THE CAFETERIA**********\n");
     printf("\t\t*****MENU*****\n");
     printf("Product             Price\n");
-    printf("[1] Pizza             50\n");
-    printf("[2] Pasta            100\n");
-    printf("[3] Momos            150\n");
-    printf("[4] Chowmein         200\n");
-    printf("[5] Fried Rice       150\n");
+    printf("[1] Pizza                150\n");
+    printf("[2] Pasta                 60\n");
+    printf("[3] Momos                 50\n");
+    printf("[4] Chowmein              60\n");
+    printf("[5] Fried Rice            70\n");
+    printf("[5] Honey Chilli          90\n");
+    
 
 
 
         repeat:
-        printf("Please Enter Your Choice: ");
-        scanf("%d",&choice);
+    printf("Please Enter Your Choice: ");
+    scanf("%d",&choice);
         
     switch (choice)
     {
          case 1:
             printf("Please Enter the number of the items: ");
             scanf("%d",&no_of_items);
-            pizza_total = pizza +(50*no_of_items);
+            pizza_total = pizza +(150*no_of_items);
             total = total+pizza_total;
             break;
     
         case 2:
             printf("Please Enter the number of the items: ");
             scanf("%d",&no_of_items);
-            pasta_total = pasta +(100*no_of_items);
+            pasta_total = pasta +(60*no_of_items);
             total = total+pasta_total;
             break;
 
         case 3:
             printf("Please Enter the number of the items: ");
             scanf("%d",&no_of_items);
-            momos_total = momos +(150*no_of_items);
+            momos_total = momos +(50*no_of_items);
             total = total+momos_total;
             break;
     
         case 4:
             printf("Please Enter the number of the items: ");
             scanf("%d",&no_of_items);
-            chowmein_total = chow+(200*no_of_items);
+            chowmein_total = chow+(60*no_of_items);
             total = total+chowmein_total;
             break;
 
         case 5:
             printf("Please Enter the number of the items: ");
             scanf("%d",&no_of_items);
-            fried_rice_total = rice+(150*no_of_items);
+            fried_rice_total = rice+(70*no_of_items);
             total = total+fried_rice_total;
             break;
-    
+
+        case 6:
+            printf("Please Enter the number of the items: ");
+            scanf("%d",&no_of_items);
+            honey_total = honey+(90*no_of_items);
+            total = total+honey_total;
+            break;
+
         default:
             printf("Sorry! The choice is Invalid.\n");
             break;
